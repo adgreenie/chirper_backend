8dc7722f09b51832c8f10b000ba2adcd2ce19d50
Adam Green <adgreenie@gmail.com>
2020-04-20 18:07:53 -0400

    deleteChirp now deletes all associated Comments

--- controllers/chirpController.js
+++ controllers/chirpController.js
@@ -1,4 +1,5 @@
 const Chirp = require("../models/Chirp")
+const Comment = require("../models/Comment")
 
 const getAllChirps = (req, res) => {
     Chirp.find({}).then(chirps => {
@@ -46,8 +47,12 @@ const updateChirp = (req, res) => {
     })
 }
 
-const deleteChirp = (req, res) => {
-    Chirp.findOneAndDelete({ _id: req.params.id }).then(chirp => {
+const deleteChirp = async (req, res) => {
+    const chirp = await Chirp.findOne({ _id: req.params.id })
+
+    await Comment.deleteMany({ _id: { $in: chirp.comments}})
+
+    chirp.delete().then(chirp => {
         res.send(`${chirp.username}'s chirp "${chirp.body}" has been deleted`)
     }).catch(err => {
         console.log(err)
